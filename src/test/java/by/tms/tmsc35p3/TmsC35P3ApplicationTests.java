4449e953e8a3e7f5b02f9829c90f85a6816a83cb
Simon Pirko <sipi7gm@gmail.com>
2025-10-29 18:32:13 +0300

    Initial commit

---
+++ src/test/java/by/tms/tmsc35p3/TmsC35P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.tmsc35p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsC35P3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
