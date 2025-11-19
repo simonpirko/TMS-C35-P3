5beb178280fc9afd7c01e8041d1bc231720fd61e
Simon Pirko <sipi7gm@gmail.com>
2025-11-19 19:07:52 +0300

    Rename SecurityConfig class to SecurityConfiguration

--- src/main/java/by/tms/tmsc35p3/config/SecurityConfiguration.java
+++ src/main/java/by/tms/tmsc35p3/config/SecurityConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfiguration {
     @Autowired
     private SecurityFilter securityFilter;
 
